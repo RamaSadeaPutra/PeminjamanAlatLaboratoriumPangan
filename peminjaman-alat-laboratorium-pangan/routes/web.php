7e367479ba1fb431f73c40adb8bfe8fb9075b53a
Fadhiladityaaaa. <fadhiladitya2810@gmail.com>
2025-12-23 23:17:59 +0700

    FEAT: Menambah Peminjaman User

--- peminjaman-alat-laboratorium-pangan/routes/web.php
+++ peminjaman-alat-laboratorium-pangan/routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ToolController;
 
 
+
 Route::get('/tools', [ToolController::class, 'index'])->name('tools.index');
 Route::get('/tools/create', [ToolController::class, 'create'])->name('tools.create');
 Route::post('/tools', [ToolController::class, 'store'])->name('tools.store');
-
 
 
 Route::get('/tools', [ToolController::class, 'index'])->name('tools.index');
@@ -17,6 +17,12 @@ Route::post('/tools', [ToolController::class, 'store'])->name('tools.store');
 Route::get('/tools/{tool}/edit', [ToolController::class, 'edit'])->name('tools.edit');
 Route::put('/tools/{tool}', [ToolController::class, 'update'])->name('tools.update');
 Route::delete('/tools/{tool}', [ToolController::class, 'destroy'])->name('tools.destroy');
+
+Route::get('/loans/create', [LoanController::class, 'create'])->name('loans.create');
+Route::post('/loans', [LoanController::class, 'store'])->name('loans.store');
+
+Route::get('/loans', [LoanController::class, 'index'])->name('loans.index');
+
 
 Route::get('/', function () {
     return view('welcome');
