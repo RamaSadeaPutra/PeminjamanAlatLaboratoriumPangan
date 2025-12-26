c7e237af5fb5b1757604446831a4c944d5cabd62
dera <deratriyadifatimah@gmail>
2025-12-26 21:38:56 +0700

    CRUD Admin

--- peminjaman-alat-laboratorium-pangan/routes/web.php
+++ peminjaman-alat-laboratorium-pangan/routes/web.php
@@ -3,71 +3,34 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ToolController;
 use App\Http\Controllers\LoanController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\User\ToolController as UserToolController;
 
-use App\Http\Controllers\AuthController;
-
-
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->name('logout');
-
-
-Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])
-        ->name('admin.dashboard');
-
-    Route::resource('tools', ToolController::class);
+// Halaman Utama
+Route::get('/', function () {
+    return view('welcome');
 });
 
-
-Route::middleware(['auth', 'user'])->prefix('user')->group(function () {
-    Route::get('/tools', [UserToolController::class, 'index'])
-        ->name('user.tools.index');
-
-    Route::post('/loans', [LoanController::class, 'store'])
-        ->name('loans.store');
-});
-
-
-
+// Autentikasi
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+// Rute khusus ADMIN
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    
+    // Management Tools untuk Admin
+    Route::resource('tools', ToolController::class);
+});
 
-
-Route::get('/user/tools', [UserToolController::class, 'index'])
-    ->name('user.tools.index');
-
-
-
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])
-    ->name('admin.dashboard');
-
-
-
-Route::get('/loans/create', [LoanController::class, 'create'])->name('loans.create');
-Route::post('/loans', [LoanController::class, 'store'])->name('loans.store');
-
-
-Route::get('/tools', [ToolController::class, 'index'])->name('tools.index');
-Route::get('/tools/create', [ToolController::class, 'create'])->name('tools.create');
-Route::post('/tools', [ToolController::class, 'store'])->name('tools.store');
-
-
-
-Route::get('/tools', [ToolController::class, 'index'])->name('tools.index');
-Route::get('/tools/create', [ToolController::class, 'create'])->name('tools.create');
-Route::post('/tools', [ToolController::class, 'store'])->name('tools.store');
-
-Route::get('/tools/{tool}/edit', [ToolController::class, 'edit'])->name('tools.edit');
-Route::put('/tools/{tool}', [ToolController::class, 'update'])->name('tools.update');
-Route::delete('/tools/{tool}', [ToolController::class, 'destroy'])->name('tools.destroy');
-
-Route::get('/loans', [LoanController::class, 'index'])->name('loans.index');
-
-
-Route::get('/', function () {
-    return view('welcome');
+// Rute khusus USER
+Route::middleware(['auth', 'user'])->prefix('user')->group(function () {
+    Route::get('/tools', [UserToolController::class, 'index'])->name('user.tools.index');
+    
+    // Peminjaman Alat
+    Route::get('/loans', [LoanController::class, 'index'])->name('loans.index');
+    Route::get('/loans/create', [LoanController::class, 'create'])->name('loans.create');
+    Route::post('/loans', [LoanController::class, 'store'])->name('loans.store');
 });
